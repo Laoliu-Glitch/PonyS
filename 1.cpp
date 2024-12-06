84aa283179823edfb19cc948e19719b0f020de2e
Laoliu-Glitch <15059512007@163.com>
2024-12-06 20:56:26 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,9 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-
-    int a,b,c;
-    
-
-    return 0;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<__gcd(a,b);
+	return 0;
 }
